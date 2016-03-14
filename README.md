8c7d3498481e3a686c95c9d2690a8d7069af9061
Andrew Ng <ajng21@gmail.com>
2016-03-13 22:56:49 -0400

    the URL fixes make me a bit hot, not gonna lie

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Get rid of HTML
 Make logo sharper
 Responsiveness on people page
-Education, Apply, Donate same size
-Zoom out on home
+Add new person to page
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ process.env.PWD = process.cwd()
 
 var express = require("express");
 var app = express();
+var serveStatic = require("serve-static")
 
-app.use(express.static(process.env.PWD));
+app.use(serveStatic(process.env.PWD, {
+  'extensions': ['html']
+}));
+
 app.get("/", function(request, response) {
-    response.send('');
+    response.send('/');
 });
 console.log('Listening on port ' + port);
 app.listen(port);
